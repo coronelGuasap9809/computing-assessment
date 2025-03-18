4e13a245c28f4b5f025fa2cb495d4807ee69dd69
Emi <emiliano.huertanunez@education.nsw.gov.au>
2025-03-18 17:16:17 +1100

    Add files via upload

---
+++ script 2.js
@@ -0,0 +1,12 @@
+function gotoAspects(){
+  location.href = "aspects.html";
+}
+
+function gotoHome(){
+  location.href = "index.html";
+}
+
+aspectsBtn.onclick = gotoAspects;
+aspectsBtnNav.onclick = gotoAspects;
+
+homeBtn.onclick = gotoHome;
