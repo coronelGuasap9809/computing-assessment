06b602966fb4a866b368437ebf652ce2f3d2163f
Emi <emiliano.huertanunez@education.nsw.gov.au>
2025-03-25 13:57:39 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-function gotoAspects(){
-  location.href = "aspects.html";
-}
-
-function gotoHome(){
-  location.href = "index.html";
-}
-
-aspectsBtn.onclick = gotoAspects;
-aspectsBtnNav.onclick = gotoAspects;
-
-homeBtn.onclick = gotoHome;
+//nothing here yet
